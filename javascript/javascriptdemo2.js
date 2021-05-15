a2cbf9889e1770c7bb27ecc0b0ffdaa83d740071
hetalpatel29 <80544632+hetalpatel29@users.noreply.github.com>
2021-05-15 17:04:31 +0530

    my practice

---
+++ javascript/javascriptdemo2.js
@@ -0,0 +1,27 @@
+function start() {
+
+    document.getElementById("horse").src = "image/e5e76fcf210d586d0c3d12cef404e01b.gif";
+    document.getElementById("horse").style.transition = "10s";
+    document.getElementById("horse").style.left = "60%";
+    document.getElementById("horse").style.transform = "rotateY(0deg)";
+
+
+
+
+}
+f
+
+function back() {
+    {
+        document.getElementById("horse").src = "image/e5e76fcf210d586d0c3d12cef404e01b.gif";
+
+        document.getElementById("horse").style.transform = "rotateY(180deg)";
+    }
+
+
+    document.getElementById("horse").style.transition = "3s";
+
+
+    document.getElementById("horse").style.left = "0%";
+
+}
